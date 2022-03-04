f11e6496f757555656b40ee9d71a883239db3702
Santiago <santiago.dgonzaga@hotmail.com>
2022-03-04 13:33:56 -0300

    Initial Commit

---
+++ MovieDb_MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDb_MVP
+//
+//  Created by Santiago del Castillo Gonzaga on 04/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
